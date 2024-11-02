fcc8ef5436476c2aeee10fc5513cfa7a473eeeef
jangy <jangyb0901@gmail.com>
2024-11-02 21:16:00 +0900

    ci : start, stop 재설정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu"
-JAR_FILE="$PROJECT_ROOT/Milky_Way_Back-0.0.1-SNAPSHOT.jar "
+JAR_FILE="$PROJECT_ROOT/Milky_Way_Back-0.0.1-SNAPSHOT.jar"
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+ APP_LOG="$PROJECT_ROOT/application.log"
+ ERROR_LOG="$PROJECT_ROOT/error.log"
+ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date +%c)
+ TIME_NOW=$(date +%c)
 
-# 이전 프로세스 종료
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-if [ -n "$CURRENT_PID" ]; then
-    echo "$TIME_NOW > 이전 프로세스 종료: $CURRENT_PID" >> $DEPLOY_LOG
-    kill -9 $CURRENT_PID
-fi
+ # JAR 파일 실행
+ echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+ nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
-
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+ CURRENT_PID=$!
+ echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,17 +3,11 @@
 PROJECT_ROOT="/home/ubuntu"
 JAR_FILE="$PROJECT_ROOT/Milky_Way_Back-0.0.1-SNAPSHOT.jar"
 
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-
-TIME_NOW=$(date +%c)
-
-# 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-
-# 프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+# JAR 파일의 PID를 찾고 종료
+PID=$(pgrep -f $JAR_FILE)
+if [ -n "$PID" ]; then
+    echo "Stopping process $PID..."
+    kill $PID
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
+    echo "No process found."
 fi
