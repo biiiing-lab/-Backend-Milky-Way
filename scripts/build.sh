7e328319c31a7f1d5882305824528b3fdd750128
jangy <jangyb0901@gmail.com>
2024-11-03 15:27:26 +0900

    ci : 자동 배포 스크립트 관련 경로 수정

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,3 +2,11 @@
 
 cd /home/ubuntu/app  # 애플리케이션 디렉토리로 이동
 ./gradlew clean build # Gradle을 사용한 빌드 명령
+
+# 빌드가 성공했는지 확인
+if [ -f "build/libs/Milky_Way_Back-0.0.1-SNAPSHOT.jar" ]; then
+    echo "Build successful, JAR file created."
+else
+    echo "Build failed, JAR file not found." >&2
+    exit 1  # 빌드 실패 시 스크립트 종료
+fi
